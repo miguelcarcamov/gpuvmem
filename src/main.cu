b4d2f2748d8e60eb82966095758aaad7f38fb846
Miguel Cárcamo <miguel.carcamo@protonmail.com>
2020-08-28 14:13:05 +0100

    fix M87 example to use entropy and laplacian priors

--- src/main.cu
+++ src/main.cu
@@ -151,8 +151,8 @@ __host__ int main(int argc, char **argv) {
 
         sy->setDevice(); // This routine sends the data to GPU memory
         Fi *chi2 = Singleton<FiFactory>::Instance().CreateFi(Chi2);
-        Fi *e = Singleton<FiFactory>::Instance().CreateFi(L1Norm);
-        Fi *l = Singleton<FiFactory>::Instance().CreateFi(TotalSquaredVariation);
+        Fi *e = Singleton<FiFactory>::Instance().CreateFi(Entropy);
+        Fi *l = Singleton<FiFactory>::Instance().CreateFi(Laplacian);
         chi2->configure(-1, 0, 0); // (penalizatorIndex, ImageIndex, imageToaddDphi)
         e->configure(0, 0, 0);
         l->configure(1, 0, 0);
