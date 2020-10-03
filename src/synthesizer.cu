d8ed4dfd4cf5cbce4283cc618e5ab3ad2f1c44b9
Miguel Cárcamo <miguel.carcamo@protonmail.com>
2020-10-03 16:30:59 +0100

    fix writeMS in synthetizer

--- src/synthesizer.cu
+++ src/synthesizer.cu
@@ -953,12 +953,14 @@ void MFS::writeResiduals()
 
         printf("Saving residuals and model to MS...\n");
         for(int d=0; d<nMeasurementSets; d++) {
+                iohandler->IocopyMS(datasets[d].name, datasets[d].oname);
                 if(!save_model_input) {
-                        iohandler->IocopyMS(datasets[d].name, datasets[d].oname);
                         iohandler->IowriteMS(datasets[d].oname, "DATA", datasets[d].fields, datasets[d].data, random_probability, false, false, false, verbose_flag);
                         iohandler->IowriteMS(datasets[d].oname, "MODEL", datasets[d].fields, datasets[d].data, random_probability, true, false, false, verbose_flag);
-                }else
+                }else{
+                        iohandler->IowriteMS(datasets[d].oname, "DATA", datasets[d].fields, datasets[d].data, random_probability, false, false, false, verbose_flag);
                         iohandler->IowriteMS(datasets[d].name, "MODEL", datasets[d].fields, datasets[d].data, random_probability, true, false, false, verbose_flag);
+                }
 
         }
 
