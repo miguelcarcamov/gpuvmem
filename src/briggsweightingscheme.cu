01078cae890ec185ad49ba87f052b6dca71f3520
Miguel Cárcamo <miguel.carcamo@protonmail.com>
2021-02-23 20:30:32 -0300

    FIX?: looking for a bug

--- src/briggsweightingscheme.cu
+++ src/briggsweightingscheme.cu
@@ -54,65 +54,8 @@ void BriggsWeightingScheme::apply(std::vector<MSDataset>& d)
                 for(int f=0; f < d[j].data.nfields; f++) {
                         for(int i=0; i < d[j].data.total_frequencies; i++) {
                                 for(int s=0; s < d[j].data.nstokes; s++) {
-
-                                        #pragma omp parallel for schedule(static, 1) num_threads(gridding) shared(g_weights) private(x, y, grid_pos_x, grid_pos_y, uvw, w)
-                                        for (int z = 0; z < d[j].fields[f].numVisibilitiesPerFreqPerStoke[i][s]; z++)
-                                        {
-                                                // First we save the original weights
-                                                uvw = d[j].fields[f].visibilities[i][s].uvw[z];
-                                                w = d[j].fields[f].visibilities[i][s].weight[z];
-
-                                                uvw.x = metres_to_lambda(uvw.x, d[j].fields[f].nu[i]);
-                                                uvw.y = metres_to_lambda(uvw.y, d[j].fields[f].nu[i]);
-                                                uvw.z = metres_to_lambda(uvw.z, d[j].fields[f].nu[i]);
-
-                                                //Apply hermitian symmetry (it will be applied afterwards)
-                                                if (uvw.x < 0.0) {
-                                                        uvw.x *= -1.0;
-                                                        uvw.y *= -1.0;
-                                                }
-
-                                                grid_pos_x = uvw.x / fabs(deltau);
-                                                grid_pos_y = uvw.y / fabs(deltav);
-                                                x = round(grid_pos_x + N / 2);
-                                                y = round(grid_pos_y + M / 2);
-
-                                                // And we grid the weights
-                                                if(x >= 0 && y >= 0 && x < M && y < M)
-                                                {
-                                                  #pragma omp critical
-                                                  {
-                                                          g_weights[N * y + x] += w;
-
-                                                  }
-                                                }
-
-
-
-                                        }
-
-                                        for(int m=0; m<M; m++) {
-                                                for(int n=N/2; n<N; n++) {
-                                                        sum_gridded_weights_squared += g_weights[N * m + n] * g_weights[N * m + n];
-                                                }
-                                        }
-
-                                        std::fill_n(g_weights.begin(), M*N, 0.0f);
-
-                                }
-                        }
-                }
-        }
-
-        average_weights = sum_gridded_weights_squared / sum_original_weights;
-        f_squared = (5.0f * powf(10.0f, -this->getRobustParam())) * (5.0f * powf(10.0f, -this->getRobustParam())) / average_weights;
-        for(int j=0; j < d.size(); j++) {
-                for(int f=0; f < d[j].data.nfields; f++) {
-                        for(int i=0; i < d[j].data.total_frequencies; i++) {
-                                for(int s=0; s < d[j].data.nstokes; s++) {
                                         d[j].fields[f].backup_visibilities[i][s].weight.resize(d[j].fields[f].numVisibilitiesPerFreqPerStoke[i][s]);
                                         xy_pos.resize(d[j].fields[f].numVisibilitiesPerFreqPerStoke[i][s]);
-
                                         #pragma omp parallel for schedule(static, 1) num_threads(gridding) shared(g_weights) private(x, y, grid_pos_x, grid_pos_y, uvw, w)
                                         for (int z = 0; z < d[j].fields[f].numVisibilitiesPerFreqPerStoke[i][s]; z++)
                                         {
@@ -136,33 +79,89 @@ void BriggsWeightingScheme::apply(std::vector<MSDataset>& d)
                                                 x = round(grid_pos_x + N / 2);
                                                 y = round(grid_pos_y + M / 2);
 
+                                                // And we grid the weights
                                                 if(x >= 0 && y >= 0 && x < M && y < M)
                                                 {
-                                                  xy_pos[z].x = x;
-                                                  xy_pos[z].y = y;
+                                                          #pragma omp critical
+                                                          {
+
+                                                                  g_weights[N * y + x] += w;
+
+                                                          }
+
+                                                }
+                                        }
+
+                                        for(int m=0; m<M; m++) {
+                                                for(int n=N/2; n<N; n++) {
+                                                        sum_gridded_weights_squared += g_weights[N * m + n] * g_weights[N * m + n];
+                                                }
+                                        }
+
+                                        std::fill_n(g_weights.begin(), M*N, 0.0f);
+
+                                }
+                        }
+                }
+        }
+
+        average_weights = sum_gridded_weights_squared / sum_original_weights;
+        f_squared = (5.0f * powf(10.0f, -this->getRobustParam())) * (5.0f * powf(10.0f, -this->getRobustParam())) / average_weights;
+        for(int j=0; j < d.size(); j++) {
+                for(int f=0; f < d[j].data.nfields; f++) {
+                        for(int i=0; i < d[j].data.total_frequencies; i++) {
+                                for(int s=0; s < d[j].data.nstokes; s++) {
+                                        #pragma omp parallel for schedule(static, 1) num_threads(gridding) shared(g_weights, xy_pos) private(x, y, grid_pos_x, grid_pos_y, uvw, w)
+                                        for (int z = 0; z < d[j].fields[f].numVisibilitiesPerFreqPerStoke[i][s]; z++)
+                                        {
+                                                // First we save the original weights
+                                                uvw = d[j].fields[f].visibilities[i][s].uvw[z];
+                                                w = d[j].fields[f].visibilities[i][s].weight[z];
+
+                                                uvw.x = metres_to_lambda(uvw.x, d[j].fields[f].nu[i]);
+                                                uvw.y = metres_to_lambda(uvw.y, d[j].fields[f].nu[i]);
+                                                uvw.z = metres_to_lambda(uvw.z, d[j].fields[f].nu[i]);
+
+                                                //Apply hermitian symmetry (it will be applied afterwards)
+                                                if (uvw.x < 0.0) {
+                                                        uvw.x *= -1.0;
+                                                        uvw.y *= -1.0;
+                                                }
+
+                                                grid_pos_x = uvw.x / fabs(deltau);
+                                                grid_pos_y = uvw.y / fabs(deltav);
+                                                x = round(grid_pos_x + N / 2);
+                                                y = round(grid_pos_y + M / 2);
+
+                                                if(x >= 0 && y >= 0 && x < M && y < M)
+                                                {
                                                   // And we grid the weights
                                                   #pragma omp critical
                                                   {
                                                           g_weights[N * y + x] += w;
+                                                          xy_pos[z].x = x;
+                                                          xy_pos[z].y = y;
 
                                                   }
                                                 }else{
-                                                  xy_pos[z].x = -1;
-                                                  xy_pos[z].y = -1;
+                                                          xy_pos[z].x = -1;
+                                                          xy_pos[z].y = -1;
                                                 }
 
                                         }
 
-                                        #pragma omp parallel for schedule(static, 1) num_threads(gridding) private(x, y)
+                                        #pragma omp parallel for schedule(static, 1) num_threads(gridding) shared(g_weights, xy_pos, f_squared) private(x, y)
                                         for (int z = 0; z < d[j].fields[f].numVisibilitiesPerFreqPerStoke[i][s]; z++)
                                         {
                                                 x = xy_pos[z].x;
                                                 y = xy_pos[z].y;
 
-                                                if(x>=0 && y>=0)
+                                                if(x >= 0 && y >= 0 && x < M && y < M)
                                                   d[j].fields[f].visibilities[i][s].weight[z] /= (1.0 + g_weights[N*y + x] * f_squared);
-                                                else
+                                                else{
+                                                  printf("Caigo aquí\n");
                                                   d[j].fields[f].visibilities[i][s].weight[z] = 0.0f;
+                                                }
                                         }
                                         std::fill_n(g_weights.begin(), M*N, 0.0f);
                                 }
