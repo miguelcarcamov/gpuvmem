692e5fbb88555720ff6e8fad3ed7bc9276528a05
Miguel Carcamo <miguel.carcamo@protonmail.com>
2022-07-09 21:17:37 +0100

    FIX: using calcFi instead of simulate

--- src/mfs.cu
+++ src/mfs.cu
@@ -1082,7 +1082,7 @@ void MFS::writeResiduals() {
     Fi* chi2 = optimizer->getObjectiveFunction()->getFiByName("Chi2");
     chi2->setCKernel(NULL);
     chi2->calcFi(image->getImage());
-    printf("Non-gridded chi2 %0.4f\n", chi2->get_fivalue());
+    printf("Non-gridded chi2 %f\n", chi2->get_fivalue());
   }
 
   for (int d = 0; d < nMeasurementSets; d++)
